5d8dcbe50152b78d074c83fc796e06aa0713124a
IrynaKazmirchuk <irakazm@gmail.com>
2023-08-09 10:26:05 +0300

    init angular-start

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { AppRoutingModule } from './app-routing.module'; // CLI imports AppRoutingModule
 import { AppComponent } from './app.component';
+import { HeaderUserComponent } from './header-user/header-user.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { FooterUserComponent } from './footer-user/footer-user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderUserComponent,
+    HomeComponent,
+    AboutComponent,
+    FooterUserComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule, // CLI adds AppRoutingModule to the AppModule's imports array
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
